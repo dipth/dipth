96e9f4dd7328384e198197b805edfb6218f1a58d
Thomas Dippel <thomas@dippel.dk>
2011-09-03 13:29:58 +0200

    Initial import

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Dipth::Application.config.secret_token = '0eadd4ddc950b421bb60235ecc0d3bfecac7f054e2aa6df500fa14eac734df18e7c2fcc82eb7be0bd05edb1d25d47d9ac8be2a2d6629322e4882a11d56dbb5b7'
