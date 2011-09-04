9c274a49cf85b875d1d76f1583433bb93717be85
Thomas Dippel <thomas@dippel.dk>
2011-09-04 13:23:50 +0200

    BlogEntriesController#index now uses full text search

--- app/controllers/blog_entries_controller.rb
+++ app/controllers/blog_entries_controller.rb
@@ -1,7 +1,14 @@
 class BlogEntriesController < ApplicationController
   
   def index
-    @blog_entries = BlogEntry.published.order("published_at desc")
+    @search = BlogEntry.search do
+      fulltext params[:search]
+      with(:published_at).less_than(Time.zone.now)
+      facet(:published_month)
+      with(:published_month, params[:month]) if params[:month].present?
+      order_by(:published_at, :desc) unless params[:search].present?
+    end
+    @blog_entries = @search.results
   end
   
 end
