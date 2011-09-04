164d24efdf9aee44e1ab6e07a78c8644317e02f1
Thomas Dippel <thomas@dippel.dk>
2011-09-04 12:28:55 +0200

    Added some specs for BlogEntry

--- spec/models/blog_entry_spec.rb
+++ spec/models/blog_entry_spec.rb
@@ -1,5 +1,43 @@
 require 'spec_helper'
 
 describe BlogEntry do
-  pending "add some examples to (or delete) #{__FILE__}"
+  
+  describe "Validation" do
+    
+    it "requires a title" do
+      blog_entry = BlogEntry.new
+      blog_entry.should have(1).error_on(:title)
+      blog_entry.title = "Test"
+      blog_entry.should have(:no).errors_on(:title)
+    end
+    
+    it "requires an excerpt" do
+      blog_entry = BlogEntry.new
+      blog_entry.should have(1).error_on(:excerpt)
+      blog_entry.excerpt = "Test"
+      blog_entry.should have(:no).errors_on(:excerpt)
+    end
+    
+    it "requires a body" do
+      blog_entry = BlogEntry.new
+      blog_entry.should have(1).error_on(:body)
+      blog_entry.body = "Test"
+      blog_entry.should have(:no).errors_on(:body)
+    end
+    
+  end
+  
+  describe "Scopes" do
+    
+    it "should not show up in the published scope unless it is published" do
+      blog_entry = FactoryGirl.create(:blog_entry)
+      BlogEntry.published.should_not include(blog_entry)
+      blog_entry.update_attribute(:published_at, Time.zone.now + 1.hour)
+      BlogEntry.published.should_not include(blog_entry)
+      blog_entry.update_attribute(:published_at, Time.zone.now)
+      BlogEntry.published.should include(blog_entry)
+    end
+    
+  end
+  
 end
