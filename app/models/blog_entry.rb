dfb5e7807a8a1c7b409e4d5e80c03759d6844e45
Thomas Dippel <thomas@dippel.dk>
2011-09-04 12:29:03 +0200

    Added some validation to BlogEntry

--- app/models/blog_entry.rb
+++ app/models/blog_entry.rb
@@ -1,5 +1,7 @@
 class BlogEntry < ActiveRecord::Base
   
+  validates_presence_of :title, :excerpt, :body
+  
   scope :published, lambda { where("published_at is not null and published_at < ?", Time.zone.now ) }
   
 end
