0711c7197e629d33c4e5c0596d00c128390d8d49
Thomas Dippel <thomas@dippel.dk>
2011-09-05 16:41:07 +0200

    Added a before-filter to the application_controller that makes sure that all traffic goes to the main domain in production

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,3 +1,14 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery
+  before_filter :ensure_domain
+  
+  APP_DOMAIN = 'dipth.com'
+  
+  # For better SEO, make sure that all traffic goes to the main domain.
+  def ensure_domain
+    if request.env['HTTP_HOST'] != APP_DOMAIN && Rails.env.production?
+      # HTTP 301 is a "permanent" redirect
+      redirect_to "http://#{APP_DOMAIN}", :status => 301
+    end
+  end
 end
