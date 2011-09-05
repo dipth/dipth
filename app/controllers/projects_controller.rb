68d389a252f4d925dc49db39943a6a99b6ae9d3a
Thomas Dippel <thomas@dippel.dk>
2011-09-05 23:30:26 +0200

    The projects are now sorted alphabetically on the projects page

--- app/controllers/projects_controller.rb
+++ app/controllers/projects_controller.rb
@@ -1,7 +1,7 @@
 class ProjectsController < ApplicationController
   
   def index
-    @project_categories = ProjectCategory.includes(:projects).order("title asc")
+    @project_categories = ProjectCategory.includes(:projects).order("project_categories.title asc, projects.title asc")
     @project_screenshots = ProjectScreenshot.includes(:project).order("random()")
   end
   
